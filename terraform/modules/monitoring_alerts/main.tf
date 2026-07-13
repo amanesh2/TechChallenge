bbf971618afc14c0b4cd559b1718bec5a46f3717
Ali Manesh <ali.manesh@outlook.com>
2026-07-13 18:59:51 +1000

    PT15M

--- terraform/modules/monitoring_alerts/main.tf
+++ terraform/modules/monitoring_alerts/main.tf
@@ -61,7 +61,7 @@ resource "azurerm_monitor_metric_alert" "sql_cpu" {
   description         = "SQL CPU threshold exceeded"
   severity            = 3
   frequency           = "PT5M"
-  window_size         = "PT10M"
+  window_size         = "PT15M"
 
   criteria {
     metric_namespace = "Microsoft.Sql/servers/databases"
